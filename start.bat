ecb58c224782ae5e1139b23fc426798502f3dbf3
lakezzun <lakezzun@users.noreply.github.com>
2026-08-29 03:59:25 +0300

    Release 0.2.13: launch via python.exe like Cardinal, not cmd.

--- start.bat
+++ start.bat
@@ -1,11 +1,3 @@
 @echo off
 cd /d "%~dp0"
-:run
-python main.py
-if exist "storage\cache\need_restart" (
-  del /f /q "storage\cache\need_restart" >nul 2>&1
-  goto run
-)
-if errorlevel 75 goto run
-echo.
-pause
+start python main.py
